c173eee6a96f933aef3828406ca035fb0e3b4029
dkdkwl <dkdkwl@naver.com>
2022-12-30 17:42:34 +0900

    리덕스 공부하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {useDispatch} from 'react-redux';
+
 
 function App() {
+  const [number,setNumber] = useState(0);
+  const dispatch = useDispatch();
+
+
+  const count = ()=>{
+    dispatch({type : "INCREMENT"})
+    setNumber(number + 1)
+  }
   return (
     <div className="App">
+      <button onClick={()=>{
+        count();
+      }}>따봉버튼</button>
+      <p>클릭시 버튼이 올라갑니다👍 {number}</p>      
     </div>
   );
 }
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,9 @@
+let initialState = {
+    number : 0
+}
+
+function reducer(state = initialState,action){
+    console.log("action",action)
+}
+
+export default reducer
