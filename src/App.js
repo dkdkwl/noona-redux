2d9eac044d142ee0123b2a54d80f85eeae549fd0
dkdkwl <dkdkwl@naver.com>
2023-01-01 18:37:51 +0900

    감소 리덕스 연습하기

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import Box from './component/Box';
 
 function App() {
   const dispatch = useDispatch();
-  const count = useSelector(state=>state.count);
-
+  let count = useSelector(state=>state.count);
+  let id = useSelector(state=>state.id);
+  let password = useSelector(state=>state.password);
 
   const increase = ()=>{
-    dispatch({type : "INCREMENT"})
+    dispatch({type : "INCREMENT",payload:{num:5}})
   }
+const login = ()=>{
+  dispatch({type:"LOGIN",payload:{id:"noona",password : "123"}})
+}
+const decrease = ()=>{
+  dispatch({type:"DECREASE"})
+}
+
   return (
     <div className="App">
       <button onClick={()=>{
         increase()
       }}>따봉버튼</button>
+      <button onClick={login}>로그인</button>
       <p>클릭시 버튼이 올라갑니다👍 {count}</p>
-      <Box/>      
+      <Box/>
+      <h1>{id},{password}</h1>
+      <button onClick={()=>{
+        decrease();
+      }}>감소</button>
     </div>
   );
 }
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,14 +1,19 @@
 let initialState = {
-    count : 0
+    count : 0,
+    id:"",
+    password : ""
 }
 
 function reducer(state = initialState,action){
-    if(action.type === "INCREMENT"){
-        return ({...state, count: state.count + 1})
-    }
+    console.log("action은 몰까",action);
+
     switch(action.type){
         case "INCREMENT" :
-            return {...state, count: state.count + 1};
+            return {...state, count: state.count  + action.payload.num};
+        case "DECREASE" :
+            return {...state, count: state.count -1};
+        case "LOGIN" :
+            return {...state,id:action.payload.id,password:action.payload.password}
             default :
             return {...state}
     }
